17cf6a66e51a8f49fc54655e4afca71d7f6e150a
prasingh26 <prasingh26@WKWIN5885086.global.publicisgroupe.net>
2018-04-26 15:33:22 +0530

    Updated Basic Thread Example

--- Exploring-Java-8/src/in/explore/core/java8/concurrency/BasicThread.java
+++ Exploring-Java-8/src/in/explore/core/java8/concurrency/BasicThread.java
@@ -1,21 +1,59 @@
 package in.explore.core.java8.concurrency;
 
+/**
+ * Basics of creating and starting a Thread in java
+ * 
+ * 1. We can directly instantiate Thread 2. Or, we can provide implementation of
+ * Runnable Interface and override run() method.
+ * 
+ * @author prasingh26
+ *
+ */
 public class BasicThread {
 
 	public static void main(String args[]) throws InterruptedException {
 
+		// Creating a new Thread here
 		Thread myThread = new Thread(new MyRunnable());
+		// setting user defined name
 		myThread.setName("MyThread-1");
+		// running a new thread
 		myThread.start();
-		System.out.println(Thread.currentThread().getName() +"  ==> Main thread name");
-		myThread.join();
-		System.out.println("Again Printed by " + Thread.currentThread().getName() + " here. After MyThead is completed ");
 		
+		
+		Thread myExtendedThread = new Thread(new MyExtednedThread());
+		myExtendedThread.setName("MyExtendedThread1Approach");
+		myExtendedThread.start();
+		
+		MyExtednedThread myExtendedThread2 = new MyExtednedThread();
+		myExtendedThread2.setName("MyExtendedThread2Approach");
+		myExtendedThread2.start();
+		
+		System.out.println(Thread.currentThread().getName() + "  ==> Main thread name");
+		/**
+		 * Joining the main thread with the myThread . the main thread will wait for its
+		 * turn for processing , till myThread is completed.
+		 */
+		myThread.join();
+		myExtendedThread.join();
+		myExtendedThread2.join();
+		
+		
+		System.out
+				.println("Again Printed by " + Thread.currentThread().getName() + " here. After MyThead is completed ");
 
 	}
 
-	private static class MyRunnable implements Runnable {
+	/**
+	 * Directly extending Thread class. Not to be used if the intention is to use
+	 * only for run() implementation.
+	 * 
+	 * @author prashantsingh
+	 *
+	 */
+	private static class MyExtednedThread extends Thread {
 
+		@Override
 		public void run() {
 
 			try {
@@ -25,11 +63,29 @@ public class BasicThread {
 				}
 			} catch (InterruptedException ie) {
 				ie.printStackTrace();
-
 			}
 
 		}
 
+	}
+
+	/**
+	 * Implementation of Runnable : To be used as a concurrent task
+	 * 
+	 * @author prashantsingh
+	 *
+	 */
+	private static class MyRunnable implements Runnable {
+		public void run() {
+			try {
+				for (int i = 1; i <= 10; i++) {
+					System.out.println(" Printed By :" + Thread.currentThread().getName() + " ==>" + i);
+					Thread.sleep(1000);
+				}
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		}
 	}
 
 }
