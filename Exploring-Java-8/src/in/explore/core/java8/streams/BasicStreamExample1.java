fa0b6a9fb671fed046733f76599da791cd734eff
prasingh26 <prasingh26@WKWIN5885086.global.publicisgroupe.net>
2018-04-26 14:37:42 +0530

    First Commit of Stream Example

---
+++ Exploring-Java-8/src/in/explore/core/java8/streams/BasicStreamExample1.java
@@ -0,0 +1,43 @@
+package in.explore.core.java8.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Basic example of using stream API.
+ * 
+ * @author prashantsingh
+ *
+ */
+public class BasicStreamExample1 {
+
+	public static void main(String args[]) {
+
+		// Filtering content from the list
+
+		List<Integer> list = createList();
+		// Dumping all even number from the stream to the console
+		System.out.println(" Even Numbers from the stream");
+		list.stream().filter(i -> i % 2 == 0).forEach(System.out::println);
+
+		System.out.println(" Double of all numbers");
+		// Double every number and print on the console
+		list.stream().map(i -> i * 2).forEachOrdered((s) -> System.out.println(s));
+
+	}
+
+	/**
+	 * Utility Method to create a list of pre-populated Integers
+	 * 
+	 * @author prasingh26
+	 * @return List<Integer> list of pre-populated Integers
+	 */
+	private static List<Integer> createList() {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 1; i <= 20; i++) {
+			list.add(i);
+		}
+		return list;
+	}
+
+}
